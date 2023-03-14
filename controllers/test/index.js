3ec2ac8cc6084462ad7a8b8e9a816d18ee4e990a
Kayden Lee <kayden@programmer.net>
2023-03-14 14:07:47 +0800

    update object

--- controllers/test/index.js
+++ controllers/test/index.js
@@ -1,12 +1,9 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-router.get(
-    "/",
-    (req, res, next) => {
-        console.log("test")
-        res.status(200).send("test")
-    }
-)
+router.get("/", (req, res, next) => {
+  console.log("test");
+  res.status(200).send({ message: "test" });
+});
 
-module.exports = router
+module.exports = router;
