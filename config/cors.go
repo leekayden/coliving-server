8b8b93c2d32f043b4acc73482b9ad2837ac5482f
Yumyummyyy <jamestiah@icloud.com>
2023-03-15 08:30:32 +0800

    fixed CORS issue

---
+++ config/cors.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func NewCorsConfig() gin.HandlerFunc {
+	frontend := os.Getenv("frontend")
+	fmt.Println(frontend)
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{frontend},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			fmt.Println(origin)
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	})
+}
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"coliving-server/config"
 	leRouter "coliving-server/router"
 	"fmt"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	godotenv.Load(".env")
 	router := gin.Default()
+	router.Use(config.NewCorsConfig())
 	leRouter.SetUpRoutes(router)
 	router.Run(":3003") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	fmt.Println("Started")
