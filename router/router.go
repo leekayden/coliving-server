29bb40a8e7a66971d6a15df3e37ca7291b3a893e
leekayden <kayden@programmer.net>
2023-03-16 14:58:04 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -2,10 +2,14 @@ package router
 
 import (
 	"coliving-server/controllers"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRoutes(r *gin.Engine) {
+	r.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "https://edenco.vercel.app/404")
+	})	
 	r.GET("/test", controllers.TestController)
 	r.GET("/appname", controllers.AppNameController)
 	r.GET("/appcurrency", controllers.AppCurrencyController)
