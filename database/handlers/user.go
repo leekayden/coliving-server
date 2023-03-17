9b30becf73d0cb76ecad4ed1e2dcd68cd45513f0
Kayden Lee <kayden@programmer.net>
2023-03-17 21:24:41 +0800

    Update user.go

--- database/handlers/user.go
+++ database/handlers/user.go
@@ -5,6 +5,7 @@ import (
 	"coliving-server/database/models"
 	"coliving-server/datatypes"
 	"fmt"
+	"time"
 )
 
 func CreateUser(user *models.User) {
@@ -31,6 +32,44 @@ func DoesEmailExist(email string) (bool, error) {
 	return result.Id != 0, nil
 }
 
+func GetUserById(id uint64) (*models.User, error) {
+	user := &models.User{}
+	err := database.Db.First(user, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func UpdateUser(user *models.User) error {
+	// Get the location of the database server
+	loc, err := time.LoadLocation(database.Db.Config.NowFunc().Location().String())
+	if err != nil {
+		return err
+	}
+
+	err = database.Db.Model(user).Updates(map[string]interface{}{
+		"fname":           user.FirstName,
+		"lname":           user.LastName,
+		"location":        user.Location,
+		"email":           user.Email,
+		"password":        user.Password,
+		"receiveAddEmail": user.ReceiveAddEmails,
+		"updated_at":      time.Now().In(loc),
+	}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteUser(id uint64) error {
+	err := database.Db.Delete(&models.User{}, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // TODO
 // Update user info
